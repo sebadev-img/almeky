9de687bbbdc0cc1339b9738422470418ff64cc79
sebastian <sebastiandev.img@gmail.com>
2023-11-26 21:55:43 -0300

    primer commit

---
+++ src/main/java/com/alkemy/alkywallet/AlkywalletApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.alkywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkywalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkywalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/alkywallet/AlkywalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.alkywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkywalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
